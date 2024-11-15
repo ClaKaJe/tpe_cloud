6f55f41f5b4cc5e888550d8b4ad48e3f74073234
ClaKaJe <137368875+ClaKaJe@users.noreply.github.com>
2024-11-15 12:52:58 +0100

    Update tpe_cloud.py

--- tpe_cloud.py
+++ tpe_cloud.py
@@ -1,45 +1,12 @@
+# This program checks if a given phrase is a palindrome. A palindrome is a word, phrase, or sequence that reads the same backward as forward. 
+# The program removes non-alphanumeric characters and ignores case before performing the check.
 
-def est_palindrome(phrase):
-     # supression d'espace et convertion en minuscule
+import re
 
-    phrase = phrase.lower()
-    phrase = phrase.replace(" ", "")
-    phrase = phrase.replace(".", "")
-    phrase = phrase.replace(",", "")
-    phrase = phrase.replace(":", "")
-    phrase = phrase.replace(";", "")
-    phrase = phrase.replace("!", "")
-    phrase = phrase.replace("?", "")
-    phrase = phrase.replace("/", "")
-    phrase = phrase.replace("(", "")
-    phrase = phrase.replace(")", "")
-    phrase = phrase.replace("'", "")
-    phrase = phrase.replace("*", "")
-    phrase = phrase.replace("-", "")
-    phrase = phrase.replace("_", "")
-    phrase = phrase.replace("{", "")
-    phrase = phrase.replace("}", "")
-    phrase = phrase.replace("[", "")
-    phrase = phrase.replace("]", "")
-    phrase = phrase.replace("|", "")
+def is_palindrome(phrase):
+    phrase = re.sub(r'[^a-zA-Z0-9]', '', phrase.lower())
 
-    #on initialise les indices 
-    debut = 0
-    fin = len(phrase) - 1 
+    print("Cette phrase est bien un palindrome !" if phrase == phrase[::-1] else "Cette phrase n'est pas un palindrome")
 
-    #on lance la boucle de comparaison
-    while debut < fin:
-        if phrase[debut] != phrase[fin]:
-            return False
-        debut += 1
-        fin -= 1
-    
-    # on retourne vrai si la boucle s'execute en entière
-    return True
 txt = input("Entrer un mot ou une phrase : ")
-txt = str(txt)
-value = est_palindrome(txt)
-if value == True:
-    print("Cette phrase est bien un palindrome !")
-else:
-    print("Cette phrase n'est pes un palindrome")
+is_palindrome(txt)
